d7be041ddeebf2a15663125490218c8070fc7c47
Udbhavi U Shetty <udbhavishetty399@gmail.com>
2026-03-03 21:12:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (10.18%) - LeetSync

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.c
@@ -0,0 +1,13 @@
+int subtractProductAndSum(int n) {
+    double sum=0,pro=1;
+    while(n>0)
+    {
+        
+        sum=sum+n%10;
+        pro=pro*(n%10);
+        n=n/10;
+
+    }
+    return pro-sum;
+    
+}
