adad58902193ae8ab02f0514887f465ac1c2a3dc
Udbhavi U Shetty <udbhavishetty399@gmail.com>
2026-03-09 20:51:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (5.85%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.c
@@ -0,0 +1,12 @@
+int lengthOfLastWord(char* s) {
+    int i=strlen(s)-1;
+    int len=0;
+    while(i>=0&&s[i]==' ')
+    i--;
+    while(i>=0&&s[i]!=' '){
+        len++;
+        i--;
+    }
+    return len;
+
+}
