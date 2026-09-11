6ad6af397e5e37069e618801402a4e999965e7fe
Udbhavi U Shetty <udbhavishetty399@gmail.com>
2026-09-11 19:33:26 +0530

    Time: 4 ms (20.27%) | Memory: 8.7 MB (20.45%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -7,15 +7,16 @@ auto init = []() {
 class Solution {
 public:
     int reverse(int x) {
-       int rev=0;
-       while(x!=0)
-       {
-        int pop=x%10;
-        if(rev>INT_MAX/10 || rev<INT_MIN/10) return 0;
+        int rev=0;
+        while(x!=0)
+        {
+            if(rev>INT_MAX/10 || rev<INT_MIN/10)
+                return 0;
+            rev =rev*10+x%10;
+            x=x/10;
+        }
+        return rev;
+
         
-        rev=rev*10+pop;
-        x=x/10;
-       }
-       return rev;
     }
 };
